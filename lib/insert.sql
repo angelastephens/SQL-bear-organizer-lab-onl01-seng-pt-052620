37d8fdb9a7e15c564db58c4596bb80497ef4c22f
Angela Stephens <angelastephens317@gmail.com>
2020-07-27 05:00:24 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 13, 'M', 'brown', 'hyper', 2);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 12, 'M', 'brown', 'calm', 2);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 11, 'M', 'brown', 'calm', 2);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'brown', 'calm', 2);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 09, 'M', 'brown', 'calm', 2);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 08, 'M', 'brown', 'calm', 2);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 07, 'M', 'brown', 'calm', 2);
